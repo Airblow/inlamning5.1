cf43e4f1f10cd0fc3fb95e8cba9b4c18b3a2576e
Airblow <143172177+Airblow@users.noreply.github.com>
2023-11-22 10:12:45 +0100

    Add project files.

---
+++ inlämning5.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace inlamning5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try { 
+                Console.WriteLine("Skriv in 5 olika namn");
+                string[] names = Console.ReadLine().Split(" ");
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(names[i]);
+
+                }
+
+            }
+            catch
+            {
+
+                Console.WriteLine("Något är fel, skrev du 5 namn?");
+            }
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
